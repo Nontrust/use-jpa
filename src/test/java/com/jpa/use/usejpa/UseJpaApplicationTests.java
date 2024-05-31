c248259223d619da909e8c8baec54f5d5d1e9007
nontrust <deed1515@naver.com>
2024-05-31 16:36:48 +0900

    :beers: [...] test코드도

---
+++ src/test/java/com/jpa/use/usejpa/UseJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.use.usejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
