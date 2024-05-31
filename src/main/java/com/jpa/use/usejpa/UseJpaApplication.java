db72a19d0f97f1da3996c0ba1efad6e3ca66b657
nontrust <deed1515@naver.com>
2024-05-31 16:36:02 +0900

    :tada: [init] JPA 강의 repository생성

---
+++ src/main/java/com/jpa/use/usejpa/UseJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.use.usejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseJpaApplication.class, args);
+    }
+
+}
